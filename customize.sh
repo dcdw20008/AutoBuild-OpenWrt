a0cf41d8e5295ee73b08261d6d47ed6d541a34af
dcdw20008 <51072216+dcdw20008@users.noreply.github.com>
2020-06-24 16:27:12 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -8,3 +8,4 @@
 #1. Modify default IP
 
 # sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/8mlzma/16mlzma/g' lede/target/linux/ar71xx/image/tiny-tp-link.mk
